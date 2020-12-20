984ef9b96e2bcb567e9262f1b6c8b7947f7aba75
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-20 21:53:17 +0900

    ○xボタンの基盤

--- QuizApp1/Controller/ViewController.swift
+++ QuizApp1/Controller/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var imageView: UIImageView!
+  
+  @IBOutlet weak var maxScoreLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
 
+  @IBAction func answer(_ sender: Any) {
+      
+    if (sender as AnyObject).tag == 1 {
+      // 丸ボタンだった場合
+      
+      
+    } else if (sender as AnyObject).tag == 2 {
+      // バツボタンだった場合
+      
+    }
+  }
+  
 }
 
