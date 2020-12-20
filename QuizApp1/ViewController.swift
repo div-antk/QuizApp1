56cba0232861f749701c12ac227248304b4aae05
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-20 21:18:29 +0900

    Initial Commit

---
+++ QuizApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp1
+//
+//  Created by Takuya Ando on 2020/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
