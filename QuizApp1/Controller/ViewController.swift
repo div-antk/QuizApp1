555a91d68f6e13a07080dd3a08688b9127fca8fe
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-22 17:45:22 +0900

    バグ修正と戻る処理を追加

--- QuizApp1/Controller/ViewController.swift
+++ QuizApp1/Controller/ViewController.swift
@@ -8,9 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  @IBOutlet weak var imageView: UIImageView!
   
+  @IBOutlet weak var imageView: UIImageView!
   @IBOutlet weak var maxScoreLabel: UILabel!
   
   var correctCount = 0
@@ -27,7 +26,7 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
+  
   // モーダルから戻ってきたときに0にしたい
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
@@ -39,45 +38,49 @@ class ViewController: UIViewController {
     // ImagesListから持ってくる
     imageView.image = UIImage(named: imagesList.list[questionNumber].imageText)
   }
-
+  
   @IBAction func answer(_ sender: Any) {
     
     if (sender as AnyObject).tag == 1 {
       // 丸ボタンだった場合
       pickedAnswer = true
-
       
     } else if (sender as AnyObject).tag == 2 {
       // バツボタンだった場合
       pickedAnswer = false
     }
     
+    
     // 回答チェック
     // pickedAnswerとImageListのCorrectOrNotの値が一致しているかどうか
-    func check() {
-      
-      let correctAnswer = imagesList.list[questionNumber].answer
-      
-      if correctAnswer == pickedAnswer {
-        correctCount += 1
-      } else {
-        wrongCount += 1
-      }
-    }
+    check()
+    // 次の問題へ
+    nextQuestions()
+  }
+  
+  func check() {
     
-    // 第何問目
-    func nextQuestions() {
+    let correctAnswer = imagesList.list[questionNumber].answer
+    
+    if correctAnswer == pickedAnswer {
+      correctCount += 1
+    } else {
+      wrongCount += 1
+    }
+  }
+  
+  // 第何問目
+  func nextQuestions() {
+    
+    if questionNumber <= 9 {
       
-      if questionNumber <= 1 {
-        
-        questionNumber += 1
-        imageView.image = UIImage(named: imagesList.list[questionNumber].imageText)
-      } else {
-        print("問題は以上")
-        
-        // 画面遷移
-        performSegue(withIdentifier: "next", sender: nil)
-      }
+      questionNumber += 1
+      imageView.image = UIImage(named: imagesList.list[questionNumber].imageText)
+    } else {
+      print("問題は以上")
+      
+      // 画面遷移
+      performSegue(withIdentifier: "next", sender: nil)
     }
   }
   
