c04f778ad51c54d2474718e280c67d32a427dd4f
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-22 18:02:22 +0900

    デリゲートメソッドとUserDefaultの定義

--- QuizApp1/Controller/NextViewController.swift
+++ QuizApp1/Controller/NextViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
+protocol NowScoreDelegate {
+  func nowScore(score:Int)
+}
+
 class NextViewController: UIViewController {
 
   @IBOutlet weak var correctLabel: UILabel!
   @IBOutlet weak var wrongLabel: UILabel!
   
+  var delegate:NowScoreDelegate?
+  
   var correctedCount = Int()
   var wrongCount = Int()
+  
+  // 以前までのスコア
+  var beforeCount = Int()
   
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +32,20 @@ class NextViewController: UIViewController {
     }
 
 
-    /*
+  @IBAction func back(_ sender: Any) {
+    
+    // もしハイスコアであれば更新
+    if beforeCount < correctedCount {
+      
+      UserDefaults.standard.set(correctedCount, forKey: "beforeCount")
+      delegate?.nowScore(score: correctedCount)
+    } else {
+      
+    }
+    
+    dismiss(animated: true, completion: nil)
+  }
+  /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
